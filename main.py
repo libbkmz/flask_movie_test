b78aa04f59b2e286a117d8d9b6f33270999b1a81
libbkmz <libbkmz.dev@gmail.com>
2019-12-01 17:34:12 +0300

    adding /movies http endpoint

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from flask import Flask, escape, request, render_template
-import requests as req
-
+from flask import Flask, render_template
 from api import DataProvider
 
 app = Flask(__name__)
 data_provider = DataProvider()
 
 
-@app.route('/')
+@app.route('/movies')
 def hello():
     films, req_times = data_provider.films
     return render_template("main.html", films=films, req_times=req_times)
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", debug=True, use_reloader=False)
