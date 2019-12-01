973813323cd728d51b774782725b76a7634ec531
libbkmz <libbkmz.dev@gmail.com>
2019-12-01 18:15:24 +0300

    changed app to create_app application factory with main movie Blueprint. Added exception handler

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, current_app, Blueprint
 from api import DataProvider
 
-app = Flask(__name__)
 data_provider = DataProvider()
+main_blueprint = Blueprint("movies", __name__)
 
 
-@app.route('/movies')
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+
+@main_blueprint.errorhandler(requests.exceptions.RequestException)
+def request_exception_handler(e):
+    return "No data from External API", 500
+
+
+@main_blueprint.route('/movies')
 def hello():
     films, req_times = data_provider.films
     return render_template("main.html", films=films, req_times=req_times)
